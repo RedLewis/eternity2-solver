c036e36eef1f2e2910cbd16d2b27b81061fa6980
andrey <jeremy1.andrey@epitech.eu>
2017-02-14 17:03:44 +0100

    move unittest into board

--- eternity2-solver/main.cpp
+++ eternity2-solver/main.cpp
@@ -9,49 +9,7 @@
 #include <assert.h>
 int main()
 {
-    ///*
-    Board tmp;
-    //wrong size + overlap
-    assert(tmp.swapRectangle(0,0,0,0,0,0) == false);
-    assert(tmp.swapRectangle(0,0,0,0,0,1) == false);
-    assert(tmp.swapRectangle(0,0,0,0,1,0) == false);
-    //rectangle same pos
-    assert(tmp.swapRectangle(0,0,0,0,1,1) == false);
-
-    //should work [easy]
-    assert(tmp.swapRectangle(1,0,0,0,1,1) == true);
-    assert(tmp.swapRectangle(0,1,0,0,1,1) == true);
-    assert(tmp.swapRectangle(0,0,1,0,1,1) == true);
-    assert(tmp.swapRectangle(0,0,0,1,1,1) == true);
-    assert(tmp.swapRectangle(0,0,1,1,1,1) == true);
-    assert(tmp.swapRectangle(1,1,0,0,1,1) == true);
-
-    assert(tmp.swapRectangle(1,1,0,0,1,1) == true);
-    assert(tmp.swapRectangle(1,1,0,1,1,1) == true);
-    assert(tmp.swapRectangle(1,1,0,2,1,1) == true);
-
-    assert(tmp.swapRectangle(1,1,1,0,1,1) == true);
-    assert(tmp.swapRectangle(1,1,1,1,1,1) == false);
-    assert(tmp.swapRectangle(1,1,1,2,1,1) == true);
-
-    assert(tmp.swapRectangle(1,1,2,0,1,1) == true);
-    assert(tmp.swapRectangle(1,1,2,1,1,1) == true);
-    assert(tmp.swapRectangle(1,1,2,2,1,1) == true);
-    //should overlap [medium]
-    // sqr shape
-    assert(tmp.swapRectangle(0,0,0,1,2,2) == false);
-    assert(tmp.swapRectangle(0,0,1,0,2,2) == false);
-    assert(tmp.swapRectangle(0,1,0,0,2,2) == false);
-    assert(tmp.swapRectangle(1,0,0,0,2,2) == false);
-    // rect shape
-    assert(tmp.swapRectangle(0,0,0,1,1,2) == false);
-    assert(tmp.swapRectangle(0,0,1,0,2,1) == false);
-    assert(tmp.swapRectangle(0,1,0,0,1,2) == false);
-    assert(tmp.swapRectangle(1,0,0,0,2,1) == false);
-    // random
-    assert(tmp.swapRectangle(8,12,9,13,1,1) == true);
-    //*/
-
+    Board::unitTestSwap();;
     ///*
     srand(time(NULL));
     int lastBestFitness = 0;
